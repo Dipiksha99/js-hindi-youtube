10e56861ef61c0daf05a20d993cc2d9f6248138e
Dipiksha99 <dipikshachaudhari99@gmail.com>
2026-07-30 08:33:46 +0000

    vari

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,23 @@
+const accountId = 144553
+let accountEmail = "dipiksha@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+
+
+// accountId = 2  // not allowed
+
+
+
+accountEmail = "dipi@gm.com"
+accountPassword = "223145263"
+accountCity = "Bangaluru"
+
+console.log(accountId);
+
+/*
+Prefer not to use var
+because of issue block code and functional code
+*/
+
+console.table([accountId, accountEmail, accountPassword, accountCity,accountState])
