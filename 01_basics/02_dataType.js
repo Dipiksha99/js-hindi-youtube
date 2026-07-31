008e73d91126acac85ecf92ca3879bc540835218
Dipiksha99 <dipikshachaudhari99@gmail.com>
2026-07-31 17:45:30 +0000

    datatypes

---
+++ 01_basics/02_dataType.js
@@ -0,0 +1,36 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3) // we are using nodejs, not browser
+
+console.log(3 
+    + 3)   // code readiability should  be high
+
+console.log("Shital")
+
+
+
+let name = "dipiksha"
+let age = 18
+let isLoggedIn = false
+
+
+//  number => 2 to power 53
+//  bigint 
+//  string =>""
+//  boolean => true/false
+//  null => standalone value
+//  undefined =>
+//  symbol =>unique
+
+
+// object
+
+console.log(typeof age); // hitesh, age ,null, undefined
+console.log(typeof undefined)
+console.log(typeof null)
+
+/*
+   ASSIGENMENT:
+   (1) SPECIFICATION DEKHANA HAI
+   https://tc39.es/ecma262/2023/multipage/ecmascript-data-types-and-values.html#sec-ecmascript-data-types-and-values
+*/
