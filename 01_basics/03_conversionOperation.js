70f1030ecb3ca2ae9dd9c3cc57bdc6b0b062b439
Dipiksha99 <dipikshachaudhari99@gmail.com>
2026-08-09 18:12:59 +0000

    con

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -35,7 +35,7 @@ let stringNumber = String(someNumber)
 
 let value = 3
 let negValue = -value
-console.log(negValue)
+// console.log(negValue)
 
 // console.log(2+2);
 // console.log(2-2);
@@ -48,11 +48,28 @@ let str1 = "hello"
 let str2 = " hitesh"
 
 let str3 = str1 + str2
-console.log(str3)
+// console.log(str3)
 
-console.log("1" + 2);
-console.log(1 + "2");
-console.log("1" + 2 + 2);
-console.log(1 + 2 + "2");
+// console.log("1" + 2);  //don't do this
+// console.log(1 + "2");    //don't do this
+// console.log("1" + 2 + 2);  //don't do this
+// console.log(1 + 2 + "2");   //don't do this
 
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(true);
+// console.log(+true);     //don't do this
+// // console.log(true+);  //error
+// console.log(+"");     //don't do this
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+// link to study
+// https://tc39.es/ecma262/2023/multipage/abstract-operations.html#sec-type-conversion
 
