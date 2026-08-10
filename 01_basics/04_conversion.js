04a12c1e98925aae55d0dcba20daf0d1ffea93c3
Dipiksha99 <dipikshachaudhari99@gmail.com>
2026-08-10 19:54:21 +0000

    convi

---
+++ 01_basics/04_conversion.js
@@ -0,0 +1,22 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log(null > 0);   //avoid
+// console.log(null == 0);  //avoid
+// console.log(null >= 0);   //avoid //comparision and equality check both are work differently
+
+// console.log(undefined == 0);   //avoid
+// console.log(undefined > 0); //avoid
+// console.log(undefined < 0);    //avoid to write this type of code
+
+//  === (strickly checking)
+
+// console.log("2" === 2);
+// console.log(2 === 2);
